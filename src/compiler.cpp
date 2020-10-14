984c8e5e80c47eab77bbec3d9ff27b1cf0a61fba
Mark Delk <jethrodaniel@gmail.com>
2020-10-14 00:07:39 -0500

    tmp, lex input

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,7 +1,11 @@
 #define FMT_HEADER_ONLY // needed to use as a header only lib
 #include <fmt/format.h>
 #include <fmt/color.h>
+#include <fmt/ranges.h>
+#include <iostream>
+#include <vector>
 
+#include <holycc/parser.hpp>
 #include <holycc/compiler.hpp>
 
 holycc::Compiler::Compiler() {
@@ -14,37 +18,56 @@ holycc::Compiler::Compiler() {
 // Takes as input a number or addition/subtraction, and exits with the
 // result of the expression.
 
+void run_lexer(std::string input) {
+  holycc::Lexer lex(input);
+  auto tokens = lex.scan_tokens();
+  for (auto t : tokens)
+    std::cout << t << "\n";
+}
+
 int holycc::Compiler::run(int argc, char **argv) {
   if (argc != 2) {
     fmt::print(stderr, fg(fmt::color::red) | fmt::emphasis::bold, "No input file provided!\n");
     return 1;
   }
 
-  char *c = argv[1];
+  auto input = std::string(argv[1]);
 
-  fmt::print(".intel_syntax noprefix\n");
-  fmt::print(".globl main\n");
-  fmt::print("main:\n");
-  fmt::print("  mov rax, {}\n", strtol(c, &c, 10));
+  try {
 
-  while (*c != 0) {
-    if (*c == '+') {
-      c++;
-      fmt::print("  add rax, {}\n", strtol(c, &c, 10));
-      continue;
-    }
+    run_lexer(input);
 
-    if (*c == '-') {
-      c++;
-      fmt::print("  sub rax, {}\n", strtol(c, &c, 10));
-      continue;
-    }
+    // holycc::Parser parser(input);
+    // auto node = parser.parse();
+    // std::cout << node << "\n";
 
-    fmt::print(stderr, fg(fmt::color::red) | fmt::emphasis::bold, "Error: {}\n", *c);
-    return 1;
+  } catch (const std::exception & ex) {
+    fmt::print("{}\n", ex.what());
   }
 
-  fmt::print("  ret\n");
+  /* fmt::print(".intel_syntax noprefix\n"); */
+  /* fmt::print(".globl main\n"); */
+  /* fmt::print("main:\n"); */
+  /* fmt::print("  mov rax, {}\n", strtol(c, &c, 10)); */
+
+  /* while (*c != 0) { */
+  /*   if (*c == '+') { */
+  /*     c++; */
+  /*     fmt::print("  add rax, {}\n", strtol(c, &c, 10)); */
+  /*     continue; */
+  /*   } */
+
+  /*   if (*c == '-') { */
+  /*     c++; */
+  /*     fmt::print("  sub rax, {}\n", strtol(c, &c, 10)); */
+  /*     continue; */
+  /*   } */
+
+  /*   fmt::print(stderr, fg(fmt::color::red) | fmt::emphasis::bold, "Error: {}\n", *c); */
+  /*   return 1; */
+  /* } */
+
+  /* fmt::print("  ret\n"); */
 
   return 0;
 }
