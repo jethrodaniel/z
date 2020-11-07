add2381661e007c62d2c6bcaa793cb9c7ed946b7
Mark Delk <jethrodaniel@gmail.com>
2020-11-06 22:32:29 -0600

    clean up spec output

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -3,8 +3,10 @@ require "./spec_helper"
 alias N = Holycc::Ast
 
 def it_parses(str, node : N::Node)
-  parser = Holycc::Parser.new(str)
-  parser.parse.should eq node
+  it str do
+    parser = Holycc::Parser.new(str)
+    parser.parse.should eq node
+  end
 end
 
 def num(val : String)
@@ -15,4 +17,7 @@ describe Holycc::Parser do
   it_parses "1",   num("1")
   it_parses "(1)", num("1")
   it_parses "1 + 2", N::BinOp.new(:+, num("1"), num("2"))
+  it_parses "1 - 2", N::BinOp.new(:-, num("1"), num("2"))
+  it_parses "1 + 2 * 3", N::BinOp.new(:+, num("1"), N::BinOp.new(:*, num("2"), num("3")))
+  it_parses "1 - 2 / 3", N::BinOp.new(:-, num("1"), N::BinOp.new(:/, num("2"), num("3")))
 end
