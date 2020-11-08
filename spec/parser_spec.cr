fe3bfda3cd66bcbbd5074b8b164ee73325ddbe48
Mark Delk <jethrodaniel@gmail.com>
2020-11-07 20:23:07 -0600

    indentation changes

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -37,24 +37,15 @@ describe Holycc::Parser do
     bi(:-,
       num("1"),
       bi(:/, num("2"), num("3")))
-  it_parses "+1;", num("1")
-  it_parses "+ 1;", num("1")
-  it_parses "-1;",
-    bi(:-,
-      num("0"),
-      num("1"))
-  it_parses "- 1;",
-    bi(:-,
-      num("0"),
-      num("1"))
-  it_parses "1 <= 2;",
-    bi(:<=,
-      num("1"),
-      num("2"))
+  it_parses "+1;",   num("1")
+  it_parses "+ 1;",  num("1")
+  it_parses "- 1;",    bi(:-, num("0"), num("1"))
+  it_parses "-1;",     bi(:-, num("0"), num("1"))
   it_parses "1 <= 2;", bi(:<=, num("1"), num("2"))
-  it_parses "1 < 2;", bi(:<, num("1"), num("2"))
+  it_parses "1 <= 2;", bi(:<=, num("1"), num("2"))
+  it_parses "1 < 2;",  bi(:<,  num("1"), num("2"))
   it_parses "1 >= 2;", bi(:>=, num("1"), num("2"))
-  it_parses "1 > 2;", bi(:>, num("1"), num("2"))
+  it_parses "1 > 2;",  bi(:>,  num("1"), num("2"))
   it_parses "1 == 2;", bi(:==, num("1"), num("2"))
-  it_parses "1== 2;", bi(:==, num("1"), num("2"))
+  it_parses "1== 2;",  bi(:==, num("1"), num("2"))
 end
