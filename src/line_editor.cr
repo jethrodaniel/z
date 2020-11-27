497b50203800ebaa0b2f89dec19e3622d8a3e425
Mark Delk <jethrodaniel@gmail.com>
2020-11-26 23:11:00 -0600

    tmp, readline work

--- src/line_editor.cr
+++ src/line_editor.cr
@@ -4,6 +4,7 @@ module Z
   class LineEditor
     @line : Int32 = 0
     @col : Int32 = 0
+    @buf : Array(Char) = [] of Char
 
     HELP = <<-MSG
     line-editor help
@@ -18,54 +19,51 @@ module Z
 
     def readline
       print @prompt, ' '
+      @buf = [] of Char
       loop do
-        buf = [] of Char
-        loop do
-          c = STDIN.raw(&.read_char).not_nil!
+        c = STDIN.raw(&.read_char).not_nil!
 
-          if !c.control?
-            # print c.inspect
-            print c
-            buf << c
-          else
-            case c
-            when '\u{7f}' # delete
-              unless buf.empty?
-                buf.pop
-                print '\b', ' ', '\b'
-              end
-            when '\r' # enter
-              puts
-              return buf.join.to_s
-            when '\e'
-              esc = [c] of Char
-              loop do
-                c = STDIN.raw(&.read_char).not_nil!
-                # puts " -> #{c.inspect}"
-                break if c == '~'
-                esc << c
-              end
-              case esc.join
-              when "\e[1" # home
-                print "\033[1G"
-                return
-              # when "\e[4" # end
-              #   print "\033[1G"
-              #   buf.each { |c| print c }
-              #   return
-              else
-                puts "\nesc => #{esc.map(&.inspect).join}"
-              end
-            when '\f' # ^l
-              buf.size.times {
-                print '\b', ' ', '\b'
-              }
-            else
-              puts "\nunknown => #{c.inspect}"
+        # handle_control_char
+        if !c.control?
+          # print c.inspect
+          print c
+          @buf << c
+        else
+          case c
+          when '\u{7f}' # delete
+            unless @buf.empty?
+              @buf.pop
+              print '\b', ' ', '\b'
             end
+          when '\r' # enter
+            puts
+            return @buf.join.to_s
+          when '\e' # TODO: handle ANSI escapes
+            esc = [c] of Char
+            loop do
+              c = STDIN.raw(&.read_char).not_nil!
+              # puts " -> #{c.inspect}"
+              break if c == '~'
+              esc << c
+            end
+            # case esc.join
+            # when "\e[1" # home
+            # when "\e[4" # end
+            # else
+            #   puts "\nesc => #{esc.map(&.inspect).join}"
+            # end
+          when '\f' # ^l
+            @buf.size.times {
+              print '\b', ' ', '\b'
+            }
+          else
+            puts "\nunknown => #{c.inspect}"
           end
         end
       end
+    end
+
+    private def handle_control_char(c)
     end
   end
 end
