90e6308fdc7397f141743df12106d1d2b8d28c5f
Mark Delk <jethrodaniel@gmail.com>
2020-11-28 19:06:47 -0600

    use vars in main.c

--- sample/main.c
+++ sample/main.c
@@ -2,10 +2,14 @@
 
 f(); // it'd be nice to drop the parens for no/default args, like HolyC
 
+
+a = b = 5;
+b = b + 1;
+
 g(24, 18); // 42
 g(1, 2);   // 3
 g(3, 4);   // 7
-g(5, 6);   // 11
+g(a, b);   // 11
 
 f();
 f();
