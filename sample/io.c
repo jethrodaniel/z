0b3fce377a53e712ba6634edfa39a6faf614b11f
Mark Delk <jethrodaniel@gmail.com>
2020-12-04 23:27:22 -0600

    get hello world working thanks to the linker

---
+++ sample/io.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void zputc(char c)
+{
+  putc(c, stdout);
+}
--- test.sh
+++
@@ -1,7 +0,0 @@
-set -x
-gcc -S sample/foo.c 
-gcc -c foo.s 
-./bin/z -c -f sample/main.c  > z.s
-gcc -c z.s 
-gcc -o a.out foo.o z.o 
-./a.out 
