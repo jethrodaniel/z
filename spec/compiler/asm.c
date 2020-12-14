b6797d3b4248f38cb548481970824d0e2076503e
Mark Delk <jethrodaniel@gmail.com>
2020-12-13 22:08:59 -0600

    fix a comment

--- spec/compiler/asm.c
+++ spec/compiler/asm.c
@@ -3,15 +3,12 @@
 // Heavily inspired by D. See https://dlang.org/spec/iasm.html
 
 main() {
-  // For now, inline assembly must be within a function.
-  //
-  // Once expressions can exist in top-level scope, assembly outside of a
-  // function will be possible.
+  // Assembly here is a first class member of the language, and uses the
+  // same tokens as the rest of the language.
   asm {
     push 24;
   }
 
-  // Note the semicolons - there is no special tokenizing going on here.
   asm { pop rax; push 42; }
 
   asm {
