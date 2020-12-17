9bf172a719f008df509076eff775e5b0e49117c0
Mark Delk <jethrodaniel@gmail.com>
2020-12-16 21:12:45 -0600

    make the sample have the start of a repl

--- sample/cc.c
+++ sample/cc.c
@@ -5,29 +5,44 @@ main() {
 repl() {
   running = 1;
 
-  while (running) {
-    putchar(63);
-    putchar(32);
+  prompt();
 
+  while (running) {
     c = getchar();
 
-    print_num(c);
-    puts();
+    if (c == 10) { // \n
+      putchar(61); // =
+      putchar(62); // >
+      putchar(32); // ' '
+      putchar(33); // !
+      // puts line
+      puts();
+      prompt();
+    }
 
-    if (c == 113)
+    if (c == 113) // q
       running = 0;
+    else {
+      print_num(c);
+      puts();
+    }
   }
 }
 
 puts() { putchar(10); }
+
+prompt() {
+  putchar(63); // ?
+  putchar(32); // ' '
+}
 
 print_num(n) {
   if (n < 0) {
     putchar(45); // -
     n = -n;
   }
-
-  if (n / 10) print_num(n / 10);
+  if (n / 10)
+    print_num(n / 10);
 
   putchar(n % 10 + 48);
 }
