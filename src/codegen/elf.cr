54bfcf6b3e2fe405571a3602b4ff9bf561310777
Mark Delk <jethrodaniel@gmail.com>
2020-12-26 21:25:51 -0600

    more elf work

--- src/codegen/elf.cr
+++ src/codegen/elf.cr
@@ -1,23 +1,35 @@
 # Write a x86_64 ELF binary
 #
 # See https://www.conradk.com/codebase/2017/05/28/elf-from-scratch/
+#
+# References:
+#
+# - `/usr/include/elf.h`
 
 module Elf
   module Index
-    MAG0 = 0
-    MAG1 = 1
-    MAG2 = 2
-    MAG3 = 3
-    CLASS = 4
-    DATA = 5
+    MAG0          = 0
+    MAG1          = 1
+    MAG2          = 2
+    MAG3          = 3
+    CLASS         = 4
+    DATA          = 5
+    VERSION       = 6
+    OSABI         = 7
+    OSABI_VERSION = 8
+    PAD = 9
   end
 
   module Const
-    MAG0  = 0x7f
-    MAG1  = 'E'.ord
-    MAG2  = 'L'.ord
-    MAG3  = 'F'.ord
-    CLASS64 = 2
+    MAG0       = 0x7f
+    MAG1       = 'E'.ord
+    MAG2       = 'L'.ord
+    MAG3       = 'F'.ord
+    CLASS64    = 2 # 64-bit objects
+    DATA2LSB   = 1 # 2's complement, litte endian
+    VERSION    = 1
+    OSABI_SYSV = 0
+    OSABI_SYSV_VERSION = 0
   end
 end
 
@@ -50,7 +62,21 @@ bin << Elf::Const::MAG1
 bin << Elf::Const::MAG2
 bin << Elf::Const::MAG3
 
+# Specify 64-bit output
 bin << Elf::Const::CLASS64
+
+# Specify endianess
+bin << Elf::Const::DATA2LSB
+
+# todo
+bin << Elf::Const::VERSION
+
+# Specify ABI
+bin << Elf::Const::OSABI_SYSV
+bin << Elf::Const::OSABI_SYSV_VERSION
+
+# Specify padding
+bin << 0
 
 File.open("a.out", "wb") do |f|
   f.write bin.to_s
