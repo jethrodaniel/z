2e1d414217c44fc4f7092729c84e88979f0f66c9
Mark Delk <jethrodaniel@gmail.com>
2020-12-26 21:01:17 -0600

    start adding elf constants

--- src/codegen/elf.cr
+++ src/codegen/elf.cr
@@ -1,9 +1,30 @@
-header = LibC::Elf_Phdr.new.tap do |h|
-  #define ET_EXEC		2		/* Executable file */
-  h.type = 2
+# Write a x86_64 ELF binary
+#
+# See https://www.conradk.com/codebase/2017/05/28/elf-from-scratch/
+
+module Elf
+  module Index
+    MAG0 = 0
+    MAG1 = 1
+    MAG2 = 2
+    MAG3 = 3
+    CLASS = 4
+    DATA = 5
+  end
+
+  module Const
+    MAG0  = 0x7f
+    MAG1  = 'E'.ord
+    MAG2  = 'L'.ord
+    MAG3  = 'F'.ord
+    CLASS64 = 2
+  end
 end
 
 elf_bin = IO::Memory.new
+
+class IO::Memory
+end
 
 # every ELF file starts with the magic number, then ASCII for 'ELF'
 #
@@ -13,8 +34,12 @@ elf_bin = IO::Memory.new
 # 00000004
 # ```
 #
-elf_bin.write_byte 0x7f
-['E', 'L', 'F'].each { |c| elf_bin << c }
+elf_bin.write_byte Elf::Const::MAG0.to_u8
+elf_bin.write_byte Elf::Const::MAG1.to_u8
+elf_bin.write_byte Elf::Const::MAG2.to_u8
+elf_bin.write_byte Elf::Const::MAG3.to_u8
+
+elf_bin.write_byte Elf::Const::CLASS64.to_u8
 
 File.open("a.out", "wb") do |f|
   f.write elf_bin.to_slice
